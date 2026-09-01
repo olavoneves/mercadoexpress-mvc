e289dc839782c38e1c0f59e6f714323bb6171452
Olavo Neves <olavo9neves@gmail.com>
2026-09-01 19:53:03 -0300

    feat: add initial project

---
+++ src/main/java/br/com/fiap/mercadoexpress/mvc/MercadoExpressMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mercadoexpress.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoExpressMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoExpressMvcApplication.class, args);
+	}
+
+}
